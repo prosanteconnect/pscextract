1e7ade81ff41238d0de1751763e50ba72f24d730
prosanteconnect <77336877+prosanteconnect@users.noreply.github.com>
2021-05-27 14:13:31 +0200

    Add files via upload

---
+++ src/main/java/fr/ans/psc/pscextract/PscextractApplication.java
@@ -0,0 +1,13 @@
+package fr.ans.psc.pscextract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PscextractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PscextractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ans/psc/pscextract/PscextractApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ans.psc.pscextract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PscextractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
