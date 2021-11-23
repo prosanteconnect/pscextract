b2d1af0f3f944bc1418b51fee65b7b0d57c0d1f6
bbonche <bbonche@henix.fr>
2021-11-23 10:22:43 +0100

    remove outrageous force gc method

--- src/main/java/fr/ans/psc/pscextract/controller/ExtractionController.java
+++ src/main/java/fr/ans/psc/pscextract/controller/ExtractionController.java
@@ -180,11 +180,4 @@ public class ExtractionController {
         }
 
     }
-
-    @PostMapping(value = "/gc")
-    public void forceGC() {
-        log.info("calling GC...");
-        System.gc();
-        log.info("gc has been called");
-    }
 }
