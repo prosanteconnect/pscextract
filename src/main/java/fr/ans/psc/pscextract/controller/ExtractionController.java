6ce45f9beb4964b55db9cc88fa7ccee9d8cb61bd
ssviridov.henix <ssviridov@henix.fr>
2022-07-20 11:58:32 +0200

    added more logging

--- src/main/java/fr/ans/psc/pscextract/controller/ExtractionController.java
+++ src/main/java/fr/ans/psc/pscextract/controller/ExtractionController.java
@@ -247,7 +247,7 @@ public class ExtractionController {
         Integer page = 0;
         List<Ps> tempPsList;
 
-        log.info("starting extraction with "+psApi.toString());
+        log.info("starting extraction on "+apiBaseUrl);
         ResponseEntity<List<Ps>> response = psApi.getPsListByPageWithHttpInfo(page,null);
         log.info("response received");
 
